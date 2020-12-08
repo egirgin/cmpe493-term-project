ec743c25e5e6cf3345adfebe39b2701344eb9473
Emre Girgin <emregirgin171@gmail.com>
2020-12-08 12:14:33 +0300

    eval metric functions added

---
+++ eval/eval.py
@@ -0,0 +1,11 @@
+def map(ground_truth, predictions):
+    # TODO
+    pass
+
+def normalized_DCG(ground_truth, predictions):
+    # TODO
+    pass
+
+def top10_precision(ground_truth, predictions):
+    # TODO
+    pass
